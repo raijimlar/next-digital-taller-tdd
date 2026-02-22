0a6d2697fec4971d36b59b0b44825dd5c9f991f2
raijimlar <raijimlar@alum.us.es>
2026-02-22 13:05:57 +0100

    feat: Implemented game logic

--- src/main/java/p3dev/TennisGame.java
+++ src/main/java/p3dev/TennisGame.java
@@ -1,16 +1,50 @@
 package p3dev;
 
+/**
+ * Clase principal del juego de tenis.
+ * Versión monolítica con lógica para puntuación normal, deuce, advantage y victoria.
+ */
 public class TennisGame {
-    public TennisGame(String s, String s1) {
-    }
 
-    public String getScore() {
-        return "";
+    private final Player player1;
+    private final Player player2;
+    private final PointNameTranslator translator;
+
+    public TennisGame(String player1Name, String player2Name) {
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
+        this.translator = new PointNameTranslator();
     }
 
     public void playerOneScores() {
+        player1.scorePoint();
     }
 
     public void playerTwoScores() {
+        player2.scorePoint();
+    }
+
+    public String getScore() {
+        int p1 = player1.getPoints();
+        int p2 = player2.getPoints();
+
+        // Win
+        if ((p1 >= 4 || p2 >= 4) && Math.abs(p1 - p2) >= 2) {
+            return (p1 > p2 ? player1.getName() : player2.getName()) + " wins";
+        }
+
+        // Deuce
+        if (p1 >= 3 && p2 >= 3) {
+            if (p1 == p2) {
+                return "Deuce";
+            }
+            return "Advantage " + (p1 > p2 ? player1.getName() : player2.getName());
+        }
+
+        // Normal scoring
+        if (p1 == p2) {
+            return translator.translate(p1) + "-All";
+        }
+        return translator.translate(p1) + "-" + translator.translate(p2);
     }
 }
