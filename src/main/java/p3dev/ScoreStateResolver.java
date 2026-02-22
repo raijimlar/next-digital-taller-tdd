55ee3066872f2b09afab9e3a6e00a5de601436cf
raijimlar <raijimlar@alum.us.es>
2026-02-22 14:16:31 +0100

    feat: Score state resolver

--- src/main/java/p3dev/ScoreStateResolver.java
+++ src/main/java/p3dev/ScoreStateResolver.java
@@ -1,10 +1,45 @@
 package p3dev;
 
 public class ScoreStateResolver {
-    public ScoreStateResolver(NormalScoreState normalState, DeuceScoreState deuceState, AdvantageScoreState advantageState, WinScoreState winState) {
+
+    private final IScoreState normalState;
+    private final IScoreState deuceState;
+    private final IScoreState advantageState;
+    private final IScoreState winState;
+
+    public ScoreStateResolver(IScoreState normalState, IScoreState deuceState,
+                              IScoreState advantageState, IScoreState winState) {
+        this.normalState = normalState;
+        this.deuceState = deuceState;
+        this.advantageState = advantageState;
+        this.winState = winState;
     }
 
     public IScoreState resolve(Player player1, Player player2) {
-        return null;
+        int p1 = player1.getPoints();
+        int p2 = player2.getPoints();
+
+        if (isWin(p1, p2)) {
+            return winState;
+        }
+        if (isDeuce(p1, p2)) {
+            return deuceState;
+        }
+        if (isAdvantage(p1, p2)) {
+            return advantageState;
+        }
+        return normalState;
+    }
+
+    private boolean isWin(int p1, int p2) {
+        return (p1 >= 4 || p2 >= 4) && Math.abs(p1 - p2) >= 2;
+    }
+
+    private boolean isDeuce(int p1, int p2) {
+        return p2 >= 3 && p1 == p2;
+    }
+
+    private boolean isAdvantage(int p1, int p2) {
+        return p1 >= 3 && p2 >= 3 && Math.abs(p1 - p2) == 1;
     }
 }
--- src/main/java/p3dev/TennisGame.java
+++ src/main/java/p3dev/TennisGame.java
@@ -1,19 +1,15 @@
 package p3dev;
 
-/**
- * Clase principal del juego de tenis.
- * Versión monolítica con lógica para puntuación normal, deuce, advantage y victoria.
- */
 public class TennisGame {
 
     private final Player player1;
     private final Player player2;
-    private final PointNameTranslator translator;
+    private final ScoreStateResolver stateResolver;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1 = new Player(player1Name);
         this.player2 = new Player(player2Name);
-        this.translator = new PointNameTranslator();
+        this.stateResolver = createDefaultResolver();
     }
 
     public void playerOneScores() {
@@ -25,26 +21,17 @@ public class TennisGame {
     }
 
     public String getScore() {
-        int p1 = player1.getPoints();
-        int p2 = player2.getPoints();
+        IScoreState state = stateResolver.resolve(player1, player2);
+        return state.getScore(player1, player2);
+    }
 
-        // Win
-        if ((p1 >= 4 || p2 >= 4) && Math.abs(p1 - p2) >= 2) {
-            return (p1 > p2 ? player1.getName() : player2.getName()) + " wins";
-        }
-
-        // Deuce
-        if (p1 >= 3 && p2 >= 3) {
-            if (p1 == p2) {
-                return "Deuce";
-            }
-            return "Advantage " + (p1 > p2 ? player1.getName() : player2.getName());
-        }
-
-        // Normal scoring
-        if (p1 == p2) {
-            return translator.translate(p1) + "-All";
-        }
-        return translator.translate(p1) + "-" + translator.translate(p2);
+    private static ScoreStateResolver createDefaultResolver() {
+        PointNameTranslator translator = new PointNameTranslator();
+        return new ScoreStateResolver(
+                new NormalScoreState(translator),
+                new DeuceScoreState(),
+                new AdvantageScoreState(),
+                new WinScoreState()
+        );
     }
 }
